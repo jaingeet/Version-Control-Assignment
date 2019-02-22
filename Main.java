4f6dadfcac5db7a1367bb49c458c491b0dcd64c4
Geetika Jain <geetikajain@Geetikas-MacBook-Air.local>
2019-02-22 16:48:43 -0600

    Added Main_v2 Binary_v2 and Linear Search

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+	public static void main(String[] args) {
+		int[] data = new int[]{1,2,3,4,5,7,16,34,67,90,124,156,378,2355};
+
+		// binary search implemenation
+		System.out.println(BinarySearch.search(data, 2355));
+
+		// linear search implemenation
+		System.out.println(LinearSearch.search(data, 2355));
+	}
+}
+ 
